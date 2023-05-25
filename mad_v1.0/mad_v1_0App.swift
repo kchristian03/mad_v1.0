407de7eff1b26384c0923123dfc0899e58ef9367
Kevin IMT'21 <kchristian03@student.ciputra.ac.id>
2023-05-25 13:41:26 +0700

    Initial Commit

---
+++ mad_v1.0/mad_v1_0App.swift
@@ -0,0 +1,17 @@
+//
+//  mad_v1_0App.swift
+//  mad_v1.0
+//
+//  Created by MacBook Pro on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct mad_v1_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
