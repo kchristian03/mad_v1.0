1d9bbeaf4cfd0bc30bc3df254c176486dba47a23
ilhamrisqi <92148866+ilhamrisqi@users.noreply.github.com>
2023-06-05 00:18:11 +0700

    pembenaran tampilan ipad

--- mad_v1.0/View/UI/DetailVIew.swift
+++ mad_v1.0/View/UI/DetailVIew.swift
@@ -8,130 +8,266 @@
 
 import SwiftUI
 
+
 struct DetailView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
     @State private var showReservationAlert = false
     @State private var selectedRectangleIndex: Int?
     var body: some View {
-        VStack {
-            Image("poster20")
-                .resizable()
-                .scaledToFill()
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-                .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
-                .edgesIgnoringSafeArea(.all)
-            
+        
+        if horizontalSizeClass == .compact{
             VStack {
-                Spacer()
-                
-                HStack {
-                    ForEach(0..<5) { _ in
-                        Image(systemName: "star.fill")
-                            .foregroundColor(.black)
-                            .padding(1)
-                    }
-                    
-                    Text("5.0")
-                        .font(.headline)
-                    
-                    Spacer()
-                    
-                    Text("2h 10m")
-                        .font(.headline)
-                        .padding(15)
-                }
-                .padding()
-                
-                HStack {
-                    VStack(alignment: .leading) {
-                        Text("Nama Film")
-                            .font(.headline)
-                        
-                        Text("Sub Nama")
-                            .font(.subheadline)
-                    }
-                    
-                    Spacer()
-                }
-                .padding(.horizontal)
+                Image("poster20")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                    .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
+                    .edgesIgnoringSafeArea(.all)
                 
                 VStack {
-                    Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, lorem ut tempus facilisis, odio leo commodo arcu, ut lacinia nunc massa sed risus.")
-                        .font(.subheadline)
-                        .frame(width: 350, height: 100)
-                }
-                .padding(.horizontal)
-                
-                ScrollView(.horizontal, showsIndicators: true) {
-                    HStack(spacing: 10) {
-                        ForEach(0..<10) { index in
-                            VStack {
-                                Rectangle()
-                                //untuk merubah background warna rectangle menjadi putih pada di tekan
-                                    .foregroundColor(selectedRectangleIndex == index ? .white : .black)
-                                    .frame(width: 60, height: 80)
-                                    .cornerRadius(10)
-                                    .padding(.bottom)
-                                    .overlay(
-                                        VStack {
-                                            Text(getDayOfWeek(for: index))
-                                                .font(.headline)
-                                            //untuk merubah background warna text menjadi hitam pada di tekan
-                                                .foregroundColor(selectedRectangleIndex == index ? .black : .white)
-                                                .multilineTextAlignment(.center)
-                                            
-                                            Text(getDate(for: index))
-                                                .font(.headline)
-                                                .foregroundColor(selectedRectangleIndex == index ? .black : .white)
-                                                .multilineTextAlignment(.center)
-                                        }
-                                            .padding(.bottom)
-                                    )
-                                    .onTapGesture {
-                                        showReservationAlert = true
-                                        selectedRectangleIndex = index
-                                    }
-                                
-                                Rectangle()
-                                //untuk merubah background warna rectangle menjadi putih pada di tekan
-                                    .foregroundColor(selectedRectangleIndex == index ? .white : .black)
-                                    .frame(width: 80, height: 40)
-                                    .cornerRadius(10)
-                                    .overlay(
-                                        VStack {
-                                            Text(getTime(for: index))
-                                                .font(.headline)
-                                            //untuk merubah background warna text menjadi hitam pada di tekan
-                                                .foregroundColor(selectedRectangleIndex == index ? .black : .white)
-                                                .multilineTextAlignment(.center)
-                                            
-                                        }
-                                    )
-                                    .onTapGesture {
-                                        showReservationAlert = true
-                                        selectedRectangleIndex = index
-                                        
-                                    }
-                                    Spacer()
-                            }
+                    Spacer()
+                    
+                    HStack {
+                        ForEach(0..<5) { _ in
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.black)
+                                .padding(1)
+                        }
+                        
+                        Text("5.0")
+                            .font(.headline)
+                        
+                        Spacer()
+                        
+                        Text("2h 10m")
+                            .font(.headline)
+                            .padding(15)
+                    }
+                    .padding()
+                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Nama Film")
+                                .font(.headline)
                             
+                            Text("Sub Nama")
+                                .font(.subheadline)
+                        }
+                        
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    
+                    VStack {
+                        Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, lorem ut tempus facilisis, odio leo commodo arcu, ut lacinia nunc massa sed risus.")
+                            .font(.subheadline)
+                            .frame(width: 350, height: 100)
+                    }
+                    .padding(.horizontal)
+                    
+                    ScrollView(.horizontal, showsIndicators: true) {
+                        HStack(spacing: 10) {
+                            ForEach(0..<10) { index in
+                                VStack {
+                                    Rectangle()
+                                    //untuk merubah background warna rectangle menjadi putih pada di tekan
+                                        .foregroundColor(selectedRectangleIndex == index ? .white : .black)
+                                        .frame(width: 60, height: 80)
+                                        .cornerRadius(10)
+                                        .padding(.bottom)
+                                        .overlay(
+                                            VStack {
+                                                Text(getDayOfWeek(for: index))
+                                                    .font(.headline)
+                                                //untuk merubah background warna text menjadi hitam pada di tekan
+                                                    .foregroundColor(selectedRectangleIndex == index ? .black : .white)
+                                                    .multilineTextAlignment(.center)
+                                                
+                                                Text(getDate(for: index))
+                                                    .font(.headline)
+                                                    .foregroundColor(selectedRectangleIndex == index ? .black : .white)
+                                                    .multilineTextAlignment(.center)
+                                            }
+                                                .padding(.bottom)
+                                        )
+                                        .onTapGesture {
+                                            showReservationAlert = true
+                                            selectedRectangleIndex = index
+                                        }
+                                    
+                                    Rectangle()
+                                    //untuk merubah background warna rectangle menjadi putih pada di tekan
+                                        .foregroundColor(selectedRectangleIndex == index ? .white : .black)
+                                        .frame(width: 80, height: 40)
+                                        .cornerRadius(10)
+                                        .overlay(
+                                            VStack {
+                                                Text(getTime(for: index))
+                                                    .font(.headline)
+                                                //untuk merubah background warna text menjadi hitam pada di tekan
+                                                    .foregroundColor(selectedRectangleIndex == index ? .black : .white)
+                                                    .multilineTextAlignment(.center)
+                                                
+                                            }
+                                        )
+                                        .onTapGesture {
+                                            showReservationAlert = true
+                                            selectedRectangleIndex = index
+                                            
+                                        }
+                                    Spacer()
+                                }
+                                
+                                
+                            }
                             
                         }
                         
+                        .padding(.vertical)
                     }
-                    
-                    .padding(.vertical)
+                    .alert(isPresented: $showReservationAlert, content: {
+                        Alert(title: Text("Reservation"), message: nil, primaryButton: .default(Text("Reservation"), action: {
+                        }), secondaryButton: .cancel())
+                    })
+                    .frame(width: 350)
                 }
-                .alert(isPresented: $showReservationAlert, content: {
-                    Alert(title: Text("Reservation"), message: nil, primaryButton: .default(Text("Reservation"), action: {
-                    }), secondaryButton: .cancel())
-                })
-                .frame(width: 350)
+                .frame(width: 400, height: 400)
+                .background(Color.white.opacity(0.5))
+                .cornerRadius(10)
+                .padding()
             }
-            .frame(width: 400, height: 400)
-            .background(Color.white.opacity(0.5))
-            .cornerRadius(10)
-            .padding()
         }
+        
+        else{
+            VStack {
+                Image("poster20")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                    .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    Spacer()
+                    
+                    HStack {
+                        ForEach(0..<5) { _ in
+                            Image(systemName: "star.fill")
+                                .foregroundColor(.black)
+                                .padding(0)
+                        }
+                        
+                        Text("5.0")
+                            .font(.headline)
+                        
+                        Spacer()
+                        
+                        Text("2h 10m")
+                            .font(.system(size: 25).bold())
+                            .padding(15)
+                    }
+                    .padding()
+                    
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text("Nama Film")
+                                .font(.system(size: 25).bold())
+                                
+                            
+                            Text("Sub Nama")
+                                .font(.subheadline)
+                        }
+                        .padding(.vertical, -10)
+                        Spacer()
+                    }
+                    .padding(.horizontal)
+                    
+                    VStack {
+                        ScrollView{
+                            Text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, lorem ut tempus facilisis, odio leo commodo arcu, ut lacinia nunc massa sed risus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, lorem ut tempus facilisis, odio leo commodo arcu, ut lacinia nunc massa sed risus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, lorem ut tempus facilisis, odio leo commodo arcu, ut lacinia nunc massa sed risus.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed tempor, lorem ut tempus facilisis, odio leo commodo arcu, ut lacinia nunc massa sed risus.")
+                                .font(.subheadline)
+                            
+                        }
+                        .frame(width: 650, height: 100)
+                        
+                    }
+                    .padding()
+                    
+                    ScrollView(.horizontal, showsIndicators: true) {
+                        HStack(spacing: 10) {
+                            ForEach(0..<10) { index in
+                                VStack {
+                                    Rectangle()
+                                    //untuk merubah background warna rectangle menjadi putih pada di tekan
+                                        .foregroundColor(selectedRectangleIndex == index ? .white : .black)
+                                        .frame(width: 60, height: 80)
+                                        .cornerRadius(10)
+                                        .padding(.bottom)
+                                        .overlay(
+                                            VStack {
+                                                Text(getDayOfWeek(for: index))
+                                                    .font(.headline)
+                                                //untuk merubah background warna text menjadi hitam pada di tekan
+                                                    .foregroundColor(selectedRectangleIndex == index ? .black : .white)
+                                                    .multilineTextAlignment(.center)
+                                                
+                                                Text(getDate(for: index))
+                                                    .font(.headline)
+                                                    .foregroundColor(selectedRectangleIndex == index ? .black : .white)
+                                                    .multilineTextAlignment(.center)
+                                            }
+                                                .padding(.bottom)
+                                        )
+                                        .onTapGesture {
+                                            showReservationAlert = true
+                                            selectedRectangleIndex = index
+                                        }
+                                    
+                                    Rectangle()
+                                    //untuk merubah background warna rectangle menjadi putih pada di tekan
+                                        .foregroundColor(selectedRectangleIndex == index ? .white : .black)
+                                        .frame(width: 80, height: 40)
+                                        .cornerRadius(10)
+                                        .overlay(
+                                            VStack {
+                                                Text(getTime(for: index))
+                                                    .font(.headline)
+                                                //untuk merubah background warna text menjadi hitam pada di tekan
+                                                    .foregroundColor(selectedRectangleIndex == index ? .black : .white)
+                                                    .multilineTextAlignment(.center)
+                                                
+                                            }
+                                        )
+                                        .onTapGesture {
+                                            showReservationAlert = true
+                                            selectedRectangleIndex = index
+                                            
+                                        }
+                                    Spacer()
+                                }
+                                
+                                
+                            }
+                            
+                        }
+                        
+                        .padding(.vertical)
+                    }
+                    .alert(isPresented: $showReservationAlert, content: {
+                        Alert(title: Text("Reservation"), message: nil, primaryButton: .default(Text("Reservation"), action: {
+                        }), secondaryButton: .cancel())
+                    }
+                    )
+                    .frame(width: 630)
+                }
+                .frame(width: 800, height: 500)
+                .background(Color.white.opacity(0.5))
+                .cornerRadius(10)
+                .padding()
+            }
+        }
+        
+        
     }
     
     
--- mad_v1.0/View/UI/TIketVIew.swift
+++ mad_v1.0/View/UI/TIketVIew.swift
@@ -9,63 +9,128 @@
 import SwiftUI
 
 struct TiketView: View {
+    @Environment(\.horizontalSizeClass) var horizontalSizeClass
+    
     var body: some View {
         
-        ZStack {
-            Color(red: 33/255, green: 36/255, blue: 44/255)
-                .edgesIgnoringSafeArea(.all)
-            VStack {
-                Text("Your Ticket")
-                    .font(.title)
-                    .foregroundColor(.white)
-                    .fontWeight(.bold)
-                    .padding()
-                
-                
+        
+        
+        if horizontalSizeClass == .compact{
+            ZStack {
+                Color(red: 33/255, green: 36/255, blue: 44/255)
+                    .edgesIgnoringSafeArea(.all)
                 VStack {
-                    Image("poster20")
-                        .resizable()
-                        .frame(width: 250, height: 400)
-                        .cornerRadius(25)
-                        .shadow(radius: 10)
-                }
-                .padding(.top)
-                HStack {
+                    Text("Your Ticket")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .padding()
                     
-                    HStack{
-                        VStack(alignment: .leading, spacing: 8) {
-                            Text("Judul Film")
-                                .font(.title)
-                                .fontWeight(.bold)
-                            Text("Tanggal")
-                                .font(.subheadline)
-                            Text("Tempat Duduk")
-                                .font(.subheadline)
+                    
+                    VStack {
+                        Image("poster20")
+                            .resizable()
+                            .frame(width: 250, height: 400)
+                            .cornerRadius(25)
+                            .shadow(radius: 10)
+                    }
+                    .padding(.top)
+                    HStack {
+                        
+                        HStack{
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text("Judul Film")
+                                    .font(.title)
+                                    .fontWeight(.bold)
+                                Text("Tanggal")
+                                    .font(.subheadline)
+                                Text("Tempat Duduk")
+                                    .font(.subheadline)
+                            }
+                            
+                            
+                            Image("barcode")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 100, height: 100)
+                                .cornerRadius(10)
+                            
+                            
+                            
                         }
                         
+                        .padding(.all)
+                        .background(Color(red: 217/255, green: 217/255, blue: 217/255))
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
                         
-                        Image("barcode")
+                    }
+                    .padding()
+                    
+                    Spacer()
+                }
+                .padding()
+            }
+        }
+        else{
+            ZStack {
+                Color(red: 33/255, green: 36/255, blue: 44/255)
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Your Ticket")
+                        .font(.system(size: 50).bold())
+                        .foregroundColor(.white)
+                        .fontWeight(.bold)
+                        .padding()
+                    
+                    
+                    VStack {
+                        Image("poster20")
                             .resizable()
-                            .aspectRatio(contentMode: .fit)
-                            .frame(width: 100, height: 100)
-                            .cornerRadius(10)
+                            .frame(width: 500, height: 800)
+                            .cornerRadius(25)
+                            .shadow(radius: 10)
+                    }
+                    .padding(.top)
+                    HStack {
+                        HStack{
+                            VStack(alignment: .leading, spacing: 8) {
+                                Text("Judul Film")
+                                    .font(.system(size: 50).bold())
+                                    .fontWeight(.bold)
+                                Text("Tanggal")
+                                    .font(.system(size: 25).bold())
+                                Text("Tempat Duduk")
+                                    .font(.system(size: 25).bold())
+                                
+                            }
+                            .frame(width: 350, height: 250)
+                            
+                            
+                            Image("barcode")
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                                .frame(width: 200, height: 200)
+                                .cornerRadius(10)
+                            
+                            
+                            
+                        }
+                        
+                        .background(Color(red: 217/255, green: 217/255, blue: 217/255))
+                        .cornerRadius(10)
+                        .shadow(radius: 10)
                         
                         
                         
                     }
-                    
-                    .padding(.all)
-                    .background(Color(red: 217/255, green: 217/255, blue: 217/255))
-                    .cornerRadius(10)
-                    .shadow(radius: 10)
-                    
+                    .padding()
                 }
-                .padding()
                 
-                Spacer()
             }
-            .padding()
         }
+        
+        
     }
 }
 
